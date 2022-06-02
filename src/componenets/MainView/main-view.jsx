5ba7557e98f35dd83ce0af60cfbbfe6f70ca8673
Fabulousman23 <andrew.makiev@gmail.com>
2022-06-02 14:11:07 -0400

    adds to mainview

---
+++ src/componenets/MainView/main-view.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+export class MainView extends react.Component {
+  render() {
+    return (
+      <div className="main-view">
+        <div>Inceptio</div>
+        <div>The Dark Knight</div>
+        <div>Gladiator</div>
+      </div>
+    );
+  }
+}
