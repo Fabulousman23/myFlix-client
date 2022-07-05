f111ebc72c60af6d029b4e4c5540ade753046229
Fabulousman23 <andrew.makiev@gmail.com>
2022-07-05 15:21:06 -0400

    login

--- src/componenets/main-view/main-view.jsx
+++ src/componenets/main-view/main-view.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import axios from "axios";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import * as images from "./data";
+import { LoginView } from "../login-view/login-view";
+
+// import * as images from "./data";
 
 export class MainView extends React.Component {
   constructor() {
@@ -10,11 +12,13 @@ export class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
+      user: null,
     };
   }
   componentDidMount() {
-    axios
-      .get("https://my-movie-app1234.herokuapp.com/movies")
+    axios.get("https://my-movie-app1234.herokuapp.com/movies");
+    console
+      .log(response.data)
       .then((response) => {
         this.setState({
           movies: response.data,
@@ -31,8 +35,17 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
     if (movies.length === 0)
       return <div className="main-view">The list is empty</div>;
     return (
