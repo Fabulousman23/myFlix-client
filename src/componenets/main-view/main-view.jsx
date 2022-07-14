032f216718c452ae4170ea2e76c0a4f2de24ce71
Fabulousman23 <andrew.makiev@gmail.com>
2022-07-14 14:21:14 -0400

    adding one Row only

--- src/componenets/main-view/main-view.jsx
+++ src/componenets/main-view/main-view.jsx
@@ -49,30 +49,30 @@ export class MainView extends React.Component {
     if (movies.length === 0)
       return <div className="main-view">The list is empty</div>;
     return (
-      <div className="main-view">
+      <Row className="main-view justify-content-md-center">
         {selectedMovie ? (
-          <Row>
-            <Col md={8}>
-              <MovieView
-                movie={selectedMovie}
-                onBackClick={newSelectedMovie => {
-                  this.setSelectedMovie(newSelectedMovie);
+          <Col md={8}>
+            <MovieView
+              movie={selectedMovie}
+              onBackClick={newSelectedMovie => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          </Col>
+        ) : (
+          movies.map(movie => (
+            <Col md={3}>
+              <MovieCard
+                key={movie._id}
+                movie={movie}
+                onMovieClick={movie => {
+                  this.setSelectedMovie(movie);
                 }}
               />
             </Col>
-          </Row>
-        ) : (
-          movies.map(movie => (
-            <MovieCard
-              key={movie._id}
-              movie={movie}
-              onMovieClick={movie => {
-                this.setSelectedMovie(movie);
-              }}
-            />
           ))
         )}
-      </div>
+      </Row>
     );
   }
 }
--- src/dist/index.fcff31a1.js
+++ src/dist/index.fcff31a1.js
@@ -25241,49 +25241,50 @@ class MainView extends _reactDefault.default.Component {
             __self: this,
             children: "The list is empty"
         }));
-        return(/*#__PURE__*/ _jsxRuntime.jsx("div", {
-            className: "main-view",
+        return(/*#__PURE__*/ _jsxRuntime.jsx(_row.Row, {
+            className: "main-view justify-content-md-center",
             __source: {
                 fileName: "src/componenets/main-view/main-view.jsx",
                 lineNumber: 52
             },
             __self: this,
-            children: selectedMovie ? /*#__PURE__*/ _jsxRuntime.jsx(_row.Row, {
+            children: selectedMovie ? /*#__PURE__*/ _jsxRuntime.jsx(_col.Col, {
+                md: 8,
                 __source: {
                     fileName: "src/componenets/main-view/main-view.jsx",
                     lineNumber: 54
                 },
                 __self: this,
-                children: /*#__PURE__*/ _jsxRuntime.jsx(_col.Col, {
-                    md: 8,
+                children: /*#__PURE__*/ _jsxRuntime.jsx(_movieView.MovieView, {
+                    movie: selectedMovie,
+                    onBackClick: (newSelectedMovie)=>{
+                        this.setSelectedMovie(newSelectedMovie);
+                    },
                     __source: {
                         fileName: "src/componenets/main-view/main-view.jsx",
                         lineNumber: 55
                     },
+                    __self: this
+                })
+            }) : movies.map((movie)=>/*#__PURE__*/ _jsxRuntime.jsx(_col.Col, {
+                    md: 3,
+                    __source: {
+                        fileName: "src/componenets/main-view/main-view.jsx",
+                        lineNumber: 64
+                    },
                     __self: this,
-                    children: /*#__PURE__*/ _jsxRuntime.jsx(_movieView.MovieView, {
-                        movie: selectedMovie,
-                        onBackClick: (newSelectedMovie)=>{
-                            this.setSelectedMovie(newSelectedMovie);
+                    children: /*#__PURE__*/ _jsxRuntime.jsx(_movieCard.MovieCard, {
+                        movie: movie,
+                        onMovieClick: (movie1)=>{
+                            this.setSelectedMovie(movie1);
                         },
                         __source: {
                             fileName: "src/componenets/main-view/main-view.jsx",
-                            lineNumber: 56
+                            lineNumber: 65
                         },
                         __self: this
-                    })
+                    }, movie._id)
                 })
-            }) : movies.map((movie)=>/*#__PURE__*/ _jsxRuntime.jsx(_movieCard.MovieCard, {
-                    movie: movie,
-                    onMovieClick: (movie1)=>{
-                        this.setSelectedMovie(movie1);
-                    },
-                    __source: {
-                        fileName: "src/componenets/main-view/main-view.jsx",
-                        lineNumber: 66
-                    },
-                    __self: this
-                }, movie._id)
             )
         }));
     }
