e33a26839822ad21eb14959640b61ff01071d806
Fabulousman23 <andrew.makiev@gmail.com>
2022-07-22 12:48:05 -0400

    add

--- src/componenets/login-view/login-view.jsx
+++ src/componenets/login-view/login-view.jsx
@@ -24,21 +24,21 @@ export function LoginView(props) {
       });
   };
 
-  return (
-    <Form>
-      <Nav />
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+  return
+  <Form>
+    <Nav />
+    <Form.Group controlId="formUsername">
+      <Form.Label>Username:</Form.Label>
+      <Form.Control type="text" placeholder="Enter Username" onChange={e => setUsername(e.target.value)} />
+    </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form >
+    <Form.Group controlId="formPassword">
+      <Form.Label>Password:</Form.Label>
+      <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+    </Form.Group>
+    <Button variant="primary" type="submit" onClick={handleSubmit}>
+      Submit
+    </Button>
+  </Form >
   );
 }
